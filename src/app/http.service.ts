4080f432f15466764adc7065fa00be3db3eba152
chirag299051 <38181042+chirag299051@users.noreply.github.com>
2019-07-23 20:01:20 -0400

    Update http.service.ts

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -8,7 +8,7 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  url: any = "http://www.omdbapi.com/?apikey=b9e96893&t="
+  url: any = "https://www.omdbapi.com/?apikey=b9e96893&t="
 
   getData(name) {
     return this.http.get(this.url + name);
